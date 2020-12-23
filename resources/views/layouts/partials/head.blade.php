715cb0822106e4b14ed2fe2295342daf25e351b4
Eddie Austin <40037317+edachecode@users.noreply.github.com>
2020-12-23 05:19:28 +0100

    layout arranged, homepage minimal setup

---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,19 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <title>Castle</title>
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/reality-icon.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/bootsnav.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/jquery.fancybox.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/owl.carousel.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/owl.transitions.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/cubeportfolio.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/settings.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/range-Slider.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/search.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/style.css') }}">
+  <link rel="icon" href="{{ asset('frontend/images/icon.png') }}">
+  </head>
