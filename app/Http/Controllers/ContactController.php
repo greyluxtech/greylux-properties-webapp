5534e845aee4e0751d1c2b738d8e82a44e4f090f
pryme0 <obochi2@gmail.com>
2020-12-30 12:28:14 +0100

    updated contactus logic

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,31 +15,33 @@ class ContactController extends Controller
     //save user contact message and send mail
     public function saveContact(Request $request) { 
         try{
-            $name = $request->input('firstname');
- $this->validate($request, [
-            'firstname' => 'required',
-            'lastname' => 'required',
-            'email' => 'required|email',
-            'subject' => 'required',
-            'phone_number' => 'required',
-            'message' => 'required'
-        ]);
-        \Mail::send('contact_email',
-             array(
-                 'firstname' => $request->get('firsname'),
-                 'lastname' => $request->get('firsname'),
-                 'email' => $request->get('email'),
-                 'subject' => $request->get('subject'),
-                 'phone_number' => $request->get('phone_number'),
-                 'user_message' => $request->get('message'),
-             ), function($message) use ($request)
-               {
-                  $message->from($request->email);
-                  $message->to('obochi2@gmail.com');
-               });
-        return response()->json(['report'=> $name]);
+            $name = $request->input('email');
+            // $indata = $request->input();
+            // $firstname =$indata[0];
+//  $this->validate($request, [
+//             'firstname' => 'required',
+//             'lastname' => 'required',
+//             'email' => 'required|email',
+//             'subject' => 'required',
+//             'phone_number' => 'required',
+//             'message' => 'required'
+//         ]);
+//         \Mail::send('contact_email',
+//              array(
+//                  'firstname' => $request->get('firsname'),
+//                  'lastname' => $request->get('firsname'),
+//                  'email' => $request->get('email'),
+//                  'subject' => $request->get('subject'),
+//                  'phone_number' => $request->get('phone_number'),
+//                  'user_message' => $request->get('message'),
+//              ), function($message) use ($request)
+//                {
+//                   $message->from($request->email);
+//                   $message->to('obochi2@gmail.com');
+//                });
+        return response()->json(['report'=>   $name]);
         }catch(Exception $e){
-            return response()->json(['report'=>$e->getMessage()]);
+            return response()->json(['report'=>$e]);
         }
     }
 
