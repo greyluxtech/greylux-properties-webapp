22aa2de331cb7875fcb30672aa56fe09bfb1d818
Favour Godwin Arua <favourvalg@gmail.com>
2021-01-10 15:52:03 +0100

    landing page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,46 @@
 <!--Slider-->
 <div class="rev_slider_wrapper">
     <div id="rev_overlaped" class="rev_slider" data-version="5.0">
-        <ul>
+        <ul class="mobile">
+            <!-- SLIDE -->
+            <li data-transition="slideleft">
+                <img src="frontend/images/home1-banner1.jpg" alt="" data-bgposition="center center" data-bgfit="cover"
+                    class="rev-slidebg">
+            </li>
+            <li data-transition="slideleft">
+                <img src="frontend/images/home1-banner2.jpg" alt="" data-bgposition="center center" data-bgfit="cover"
+                    class="rev-slidebg">
+            </li>
+            <li data-transition="slideleft">
+                <img src="frontend/images/home1-banner3.jpg" alt="" data-bgposition="center center" data-bgfit="cover"
+                    class="rev-slidebg">
+            </li>
+            <div class="tp-static-layers">
+                <div class="tp-caption tp-static-layer" id="slide-37-layer-2" data-x="['left','left','left','left']"
+                    data-hoffset="['50','50','50','50']" data-y="['bottom','bottom','bottom','bottom']"
+                    data-voffset="['230','180','150','100']" data-whitespace="nowrap"
+                    data-visibility="['on','on','on','on']" data-fontsize="['48','48','28','28']" data-start="500"
+                    data-responsive_offset="on" data-basealign="slide" data-startslide="0" data-endslide="5"
+                    style="z-index: 5;">
+                    <h1><span class="t_white">We Can Find just Right <br>Property for You.</span></h1>
+                </div>
+                {{-- <div class="tp-caption tp-static-layer" id="slide-37-layer-2" data-x="['left','left','left','left']"
+                    data-hoffset="['50','50','50','50']" data-y="['bottom','bottom','bottom','bottom']"
+                    data-voffset="['150','100','120','120']" data-whitespace="nowrap"
+                    data-visibility="['on','on','on','on']" data-start="500" data-basealign="slide"
+                    data-startslide="0" data-endslide="5" style="z-index: 5;">
+                </div> --}}
+                <div class="tp-caption tp-static-layer" id="slide-37-layer-2" data-x="['left','left','left','left']"
+                    data-hoffset="['50','50','50','50']" data-y="['bottom','bottom','bottom','bottom']"
+                    data-voffset="['60','60','60','60']" data-whitespace="nowrap"
+                    data-visibility="['on','on','on','on']" data-start="500" data-basealign="slide"
+                    data-startslide="0" data-endslide="5" style="z-index: 5;"><a href="listing1.html"
+                        class="btn-white border_radius uppercase">view Properties</a>
+                </div>
+            </div>
+        </ul>
+
+        <ul class="desktop">
             <!-- SLIDE -->
             <li data-transition="fade">
                 <img src="frontend/images/home1-banner1.jpg" alt="" data-bgposition="center center" data-bgfit="cover"
